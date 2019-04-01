5cd97143c5c5ff0d0aa0758702b6a8bdfddc083a
Umar--Khan <umarkhandev@gmail.com>
2019-04-01 14:00:39 +0100

    Done.

--- app/application.rb
+++ app/application.rb
@@ -11,16 +11,31 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
-    elsif req.path.match(/cart/)
-        @@cart.each do |x|
-        resp.write "#{x}\n"
-      end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.empty? 
+        resp.write "Your cart is empty"
+      else req.path.match(/cart/)
+          @@cart.each do |x|
+          resp.write "#{x}\n"
+          end
+        end
+
+      elsif req.path.match(/add/)
+        add_item = req.params["item"]
+        if @@items.include? add_item
+          @@cart << add_item
+          resp.write "added #{add_item}"
+        else
+          resp.write "We don't have that item"
+        end
       else
         resp.write "Path Not Found"
       end
+
+
     resp.finish
   end
 
@@ -63,7 +78,7 @@ end
  
 #     resp.finish
 #   end
-end
+# end
 
 #TODO Emails 
 # ! jajd
