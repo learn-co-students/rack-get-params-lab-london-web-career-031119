d450c886e6fa329becd060631f00914ede7e0945
Umar--Khan <umarkhandev@gmail.com>
2019-04-01 13:37:01 +0100

    test

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = ["Apples", "Oranges"]
 
   def call(env)
     resp = Rack::Response.new
@@ -10,13 +11,16 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+    elsif req.path.match(/cart/)
+        @@cart.each do |x|
+        resp.write "#{x}\n"
+      end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    else
-      resp.write "Path Not Found"
-    end
-
+      else
+        resp.write "Path Not Found"
+      end
     resp.finish
   end
 
@@ -27,4 +31,42 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+
 end
+
+
+# class Application
+ 
+#   @@items = ["Apples","Carrots","Pears"]
+ 
+#   def call(env)
+#     resp = Rack::Response.new
+#     req = Rack::Request.new(env)
+ 
+#     if req.path.match(/items/)
+#       @@items.each do |item|
+#         resp.write "#{item}\n"
+#       end
+#     elsif req.path.match(/search/)
+ 
+#       search_term = req.params["q"]
+ 
+#       if @@items.include?(search_term)
+#         resp.write "#{search_term} is one of our items"
+#       else
+#         resp.write "Couldn't find #{search_term}"
+#       end
+ 
+#     else
+#       resp.write "Path Not Found"
+#     end
+ 
+#     resp.finish
+#   end
+end
+
+#TODO Emails 
+# ! jajd
+# ? 
+#////fernando
+# normal
