7b89e1d9b60536a1e2ca5f4b0febcf8ca7f94b7c
azamkhanuk <azamkhandev@gmail.com>
2019-04-01 13:58:51 +0100

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,13 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+
+    
 
     if req.path.match(/items/)
       @@items.each do |item|
@@ -13,10 +16,45 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    # ! Viewing the cart and if empty returns string. CHECK CART IS EMPTY FIRST!
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+          resp.write "Your cart is empty"
+        else
+          req.path.match(/cart/)
+            @@cart.each do |cart|
+          resp.write "#{cart}\n"
+          end
+      end
+    # ! Adds only items within the item list 
+      elsif req.path.match(/add/)
+        # @@cart << @@items
+        add_item = req.params["item"]
+        if @@items.include? add_item
+          @@cart << add_item
+          resp.write "added #{add_item}"
+        else
+          resp.write "We don't have that item"
+        end
     else
       resp.write "Path Not Found"
     end
 
+    ############################## ? Testing code 
+
+    # if req.path.match(/cart/)
+    #   @@cart.each do |cart|
+    #     resp.write "#{cart}\n"
+    #   end
+    # else
+    #   resp.write "Your cart is empty"
+    # end
+
+  # elsif @@cart.empty?
+  #   resp.write "Your cart is empty"
+
+    ############################## ?
+    # * returns the final response 
     resp.finish
   end
 
